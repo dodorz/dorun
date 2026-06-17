f7acd5408beb0e3b97296f817c8a55ad3f99c0cf
dodo <perfectdodo@gmail.com>
2026-06-17 15:21:25 +0800

    Use DORUN_VERSION_STRING macro for FileVersion and ProductVersion in RC file

--- src/version.h
+++ src/version.h
@@ -4,3 +4,7 @@
 #define DORUN_VERSION_MINOR 2
 #define DORUN_VERSION_PATCH 4
 #define DORUN_VERSION_BUILD 36
+
+#define DORUN_VERSION_STR_IMPL(major, minor, patch, build) #major "." #minor "." #patch "." #build
+#define DORUN_VERSION_STR(major, minor, patch, build) DORUN_VERSION_STR_IMPL(major, minor, patch, build)
+#define DORUN_VERSION_STRING DORUN_VERSION_STR(DORUN_VERSION_MAJOR, DORUN_VERSION_MINOR, DORUN_VERSION_PATCH, DORUN_VERSION_BUILD)
