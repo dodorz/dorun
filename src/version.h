c4122bff9829293b431588ce915f611e67accfb9
dodo <perfectdodo@gmail.com>
2026-06-17 14:18:47 +0800

    Use Segoe UI as default font instead of Segoe UI Variable

--- src/version.h
+++ src/version.h
@@ -2,5 +2,9 @@
 
 #define DORUN_VERSION_MAJOR 0
 #define DORUN_VERSION_MINOR 2
-#define DORUN_VERSION_PATCH 3
-#define DORUN_VERSION_BUILD 35
+#define DORUN_VERSION_PATCH 4
+#define DORUN_VERSION_BUILD 36
+
+#define DORUN_VERSION_STR_IMPL(major, minor, patch, build) #major "." #minor "." #patch "." #build
+#define DORUN_VERSION_STR(major, minor, patch, build) DORUN_VERSION_STR_IMPL(major, minor, patch, build)
+#define DORUN_VERSION_STRING DORUN_VERSION_STR(DORUN_VERSION_MAJOR, DORUN_VERSION_MINOR, DORUN_VERSION_PATCH, DORUN_VERSION_BUILD)
