9df47f0f7b359039f261f65ec4b9b6554f860372
dodo <perfectdodo@gmail.com>
2026-06-19 16:16:24 +0800

    fix: use git describe to find previous tag for accurate release detection

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 #define DORUN_VERSION_MAJOR 0
 #define DORUN_VERSION_MINOR 2
 #define DORUN_VERSION_PATCH 4
-#define DORUN_VERSION_BUILD 36
+#define DORUN_VERSION_BUILD 38
 
 #define DORUN_VERSION_STR_IMPL(major, minor, patch, build) #major "." #minor "." #patch "." #build
 #define DORUN_VERSION_STR(major, minor, patch, build) DORUN_VERSION_STR_IMPL(major, minor, patch, build)
